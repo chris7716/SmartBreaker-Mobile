5f83f1f9561492befc0ff6ac49f64e1d919e6ed6
Hasitha Kaushan <hkraclh@gmail.com>
2019-02-26 16:23:40 +0530

    Analysis page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' }
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'analysis', loadChildren: './analysis/analysis.module#AnalysisPageModule' }
 ];
 @NgModule({
   imports: [
