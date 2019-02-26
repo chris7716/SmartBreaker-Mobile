b03c7a7372d723a3382e17eddb1d482dae586713
Hasitha Kaushan <hkraclh@gmail.com>
2019-02-26 16:39:19 +0530

    Config page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'analysis', loadChildren: './analysis/analysis.module#AnalysisPageModule' }
+  { path: 'analysis', loadChildren: './analysis/analysis.module#AnalysisPageModule' },
+  { path: 'config', loadChildren: './config/config.module#ConfigPageModule' }
 ];
 @NgModule({
   imports: [
