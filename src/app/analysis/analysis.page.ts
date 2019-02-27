7ea0a3d4b104b017773f4e332489f8c368e08592
Hasitha Kaushan <hkraclh@gmail.com>
2019-02-27 17:21:28 +0530

    Charts added to analysis

--- src/app/analysis/analysis.page.ts
+++ src/app/analysis/analysis.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { Chart } from 'chart.js';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-analysis',
@@ -7,9 +10,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalysisPage implements OnInit {
 
+  @ViewChild('lineCanvas') lineCanvas;
+  @ViewChild('doughnutCanvas') doughnutCanvas;
+
+  doughnutChart: any;
+  lineChart: any;
+
+  subscription: Subscription;
+
   constructor() { }
 
   ngOnInit() {
+
+    //emit value in sequence every 10 second
+    const source = interval(500);
+    //const text = 'Your Text Here';
+    this.subscription = source.subscribe(val => this.getChartData());
+
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+
+      type: 'doughnut',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }]
+      }
+
+    });
+
+  }
+
+  getChartData(){
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+
+      type: 'line',
+      data: {
+        animation : {
+          duration: 0
+        },
+        labels: ["1", "2", "3", "4", "5", "6", "7"],
+        datasets: [
+          {
+            label: "Magnitude",
+            fill: false,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [this.random(), this.random(), this.random(), this.random(), this.random(), this.random(), this.random()],
+            spanGaps: false,
+          }
+        ]
+      }
+
+    });
+
+  }
+
+  private random() {
+    return Math.round(Math.random() * 100);
   }
 
 }
